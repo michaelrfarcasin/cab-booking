89c2a4dbf0ab5637c46214bf9ed9456e47163442
michaelrfarcasin <rockdeworld@gmail.com>
2025-10-02 16:56:08 -0600

    Use environment variables for frontend

--- frontend/src/components/api/AuthApiService.js
+++ frontend/src/components/api/AuthApiService.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const AUTH_API_URL = process.env.REACT_APP_AUTH_API_URL || 'http://localhost:8081'
+
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8081'
+    baseURL: AUTH_API_URL
 })
 
 export const executeJwtAuthenticationService = (username, password) => apiClient.post(`/user/generateToken`, {
--- frontend/src/components/api/BookingApiService.js
+++ frontend/src/components/api/BookingApiService.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const BOOKING_API_URL = process.env.REACT_APP_BOOKING_API_URL || 'http://localhost:8082'
+
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8082'
+    baseURL: BOOKING_API_URL
 })
 
 export const getBookingsForUser = () => apiClient.get(`booking`)
